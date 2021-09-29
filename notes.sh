a22655f84b049ea7549a54e527082e61af199a82
ps173 <prathamsharma173@gmail.com>
2021-09-29 18:26:11 +0530

    oops

--- notes.sh
+++ notes.sh
@@ -9,10 +9,10 @@ FULL_PATH="$HOME/Documents/personal-notes/$TITLE.md"
 if [ -f "$FULL_PATH" ]; then
     echo "$FULL_PATH exists."
 else
-    echo -e "# $_file \n">> $FULL_PATH
+    echo -e "# $TITLE \n">> $FULL_PATH
     date >> $FULL_PATH
     echo -e "\n">> $FULL_PATH
 fi
 
 # Open in your friendly editor
-$EDITOR $FULL_PATH
+vim $FULL_PATH
